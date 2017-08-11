2513f463882f196ab7998b900a88e6b480e49093
Shiling2ndpara <shilling2ndpara@gmail.com>
2017-08-11 17:06:29 +0300

    fix end of file

--- javascript/bicycle.js
+++ javascript/bicycle.js
@@ -5,18 +5,14 @@
 
 window.onload = function () {
     if (typeof(Event) === 'function') {
-
         //alert('modern browsers');
         window.dispatchEvent(new Event('resize'));
     } else {
-
         //alert('IE and other old browsers');
         // causes deprecation warning on modern browsers
         var evt = window.document.createEvent('UIEvents');
         evt.initUIEvent('resize', true, false, window, 0);
         window.dispatchEvent(evt);
     }
-}
-
-
+};
 
