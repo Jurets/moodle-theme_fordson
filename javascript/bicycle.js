e89cd00dd224172dead5c12d7176e8a9b3479de6
Shiling2ndpara <shilling2ndpara@gmail.com>
2017-08-11 16:14:46 +0300

    run window resize event for the quiz/review

---
+++ javascript/bicycle.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Shilling on 11.08.2017.
+ */
+//alert('HELLO DUCK !!!');
+
+window.onload = function () {
+    if (typeof(Event) === 'function') {
+
+        //alert('modern browsers');
+        window.dispatchEvent(new Event('resize'));
+    } else {
+
+        //alert('IE and other old browsers');
+        // causes deprecation warning on modern browsers
+        var evt = window.document.createEvent('UIEvents');
+        evt.initUIEvent('resize', true, false, window, 0);
+        window.dispatchEvent(evt);
+    }
+}
+
+
+
