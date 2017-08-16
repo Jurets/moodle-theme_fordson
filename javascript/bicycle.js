cd8ff413cc93373c01e462d2abcefc290acf2631
Shiling2ndpara <shilling2ndpara@gmail.com>
2017-08-16 13:03:56 +0300

    resize content block on quiz/review page for Safari browser

--- javascript/bicycle.js
+++ javascript/bicycle.js
@@ -1,7 +1,6 @@
 /**
  * Created by Shilling on 11.08.2017.
  */
-//alert('HELLO DUCK !!!');
 
 window.onload = function () {
     if (typeof(Event) === 'function') {
@@ -13,6 +12,27 @@ window.onload = function () {
         var evt = window.document.createEvent('UIEvents');
         evt.initUIEvent('resize', true, false, window, 0);
         window.dispatchEvent(evt);
+
+        //var contentElem = document.getElementById('region-main');
+        //var computedStyle = getComputedStyle(contentElem);
+        //alert(computedStyle.width );
+
+        // added by Yulia on 16/08/2017
+        var thisElem = document.querySelector('#region-main'),
+            thisParent = thisElem.parentElement,
+            allChildren = thisParent.children,
+            thisParentWidth = thisParent.offsetWidth,
+            allChildrenWidth = 0,
+            thisElWidth;
+
+        for (var i = 0; i < allChildren.length; i++) {
+            if(allChildren[i] !== thisElem) {
+                allChildrenWidth += allChildren[i].offsetWidth;
+            }
+        }
+
+        thisElWidth = thisParentWidth - allChildrenWidth;
+        thisElem.style.width = thisElWidth - 60 +'px';
     }
 };
 
