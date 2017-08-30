c408ce58cc6fabdb4b15ea44962e2af5d3a3ae83
Julyjul1987 <julsjul1403@gmail.com>
2017-08-30 12:05:25 +0300

    https://academysmart.teamwork.com/#tasks/8229523 Smooth scroll to anchor

--- javascript/anchor.js
+++ javascript/anchor.js
@@ -1,29 +1,23 @@
 window.onload = function () {
-    
     var linkNav = document.querySelectorAll('[href*="#section-"]'),
-        V = 2; // скорость
-    for (var i = 0; i < linkNav.length; i++) {
-        linkNav[i].onclick = function () {
-            var w = window.pageYOffset, hashElem = this.href.toString().indexOf('#'),
+            V = 1; // speed
+        for (var i = 0; i < linkNav.length; i++) {
+          linkNav[i].onclick = function(){
+            var windScroll = window.pageYOffset,
+                hashElem = this.href.toString().indexOf('#'),
                 hash = this.href.slice(hashElem),
-                t = document.querySelector(hash).getBoundingClientRect().top,
-                head = document.querySelector('header[role="banner"]').offsetHeight,
+                topOfElem = document.querySelector(hash).getBoundingClientRect().top,
+                headerHeigtn = document.querySelector('header[role="banner"]').offsetHeight,
                 start = null;
             requestAnimationFrame(step);
             function step(time) {
-                if (start === null) start = time;
-                var progress = time - start,
-                    r = (t <= head ? Math.max(w - progress / V, w + t - head) : Math.min(w + progress / V, w + t - head));
-                window.scrollTo(0, r);
-                if (r != w + t) {
-                    requestAnimationFrame(step)
-                } else {
-                    location.hash = hash
-                }
+              if (start === null) start = time;
+              var progress = time - start,
+                  scrollToEl = (topOfElem <= headerHeigtn ? Math.max(windScroll - progress/V, windScroll + topOfElem - headerHeigtn) : Math.min(windScroll + progress/V, windScroll + topOfElem - headerHeigtn));
+              window.scrollTo(0,scrollToEl);
+              if (scrollToEl != windScroll + topOfElem - headerHeigtn) {requestAnimationFrame(step)}
             }
-
             return false;
+          }
         }
-    }
-
-};
+ };
